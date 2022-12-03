5768b3e8a01d337d60abead109b79b322b756ab6
Yest Izit <hi@dmitry.email>
2022-12-03 13:25:05 +0400

    feat: use trigram for string similarity calculations

--- src/utils/str.rs
+++ src/utils/str.rs
@@ -1,6 +1,6 @@
-use strsim::jaro_winkler;
+use trigram::similarity;
 
-pub fn is_same_first_char(first_str: &str, second_str: &str) -> bool {
+fn is_same_first_char(first_str: &str, second_str: &str) -> bool {
     let second_str_first_char = match second_str.chars().next() {
         Some(c) => c,
         None => return false,
@@ -8,8 +8,33 @@ pub fn is_same_first_char(first_str: &str, second_str: &str) -> bool {
     first_str.starts_with(second_str_first_char)
 }
 
-pub fn is_similar(first_str: &str, second_str: &str) -> bool {
-    is_same_first_char(first_str, second_str) && jaro_winkler(first_str, second_str) >= 0.95
+fn is_same_last_char(first_str: &str, second_str: &str) -> bool {
+    let second_str_last_char = match second_str.chars().last() {
+        Some(c) => c,
+        None => return false,
+    };
+    first_str.ends_with(second_str_last_char)
+}
+
+fn starts_ends_same(first_str: &str, second_str: &str) -> bool {
+    is_same_first_char(first_str, second_str) && is_same_last_char(first_str, second_str)
+}
+
+fn is_similar_length(str: &str, str_to_check: &str) -> bool {
+    str.chars().count().abs_diff(str_to_check.chars().count()) <= 2
+}
+
+pub fn is_similar(str: &str, str_to_check: &str) -> bool {
+    if !starts_ends_same(str, str_to_check) || !is_similar_length(str, str_to_check) {
+        return false;
+    }
+    println!(
+        "{} - {}, {}",
+        similarity(str, str_to_check),
+        str,
+        str_to_check
+    );
+    similarity(str, str_to_check) >= 0.55
 }
 
 #[cfg(test)]
@@ -29,13 +54,17 @@ mod str_tests {
 
     #[test]
     fn test_is_similar() {
-        assert!(is_similar("абберация", "абберации"));
         assert!(is_similar("абберация", "аберация"));
-        assert!(is_similar("идеология", "идеологии"));
-        assert!(is_similar("социальный", "социальные"));
+
         assert!(!is_similar("социальный", "асоциальный"));
         assert!(!is_similar("кек", "кеке"));
         assert!(!is_similar("запостить", "захостить"));
         assert!(!is_similar("запостить", "запустить"));
+        assert!(!is_similar("кант", "канат"));
+        assert!(!is_similar("систематик", "систематический"));
+        assert!(!is_similar("систематик", "систематика"));
+        assert!(!is_similar("систематика", "систематик"));
+        assert!(!is_similar("систематик", "синематик"));
+        assert!(!is_similar("систематик", "системник"));
     }
 }
